cf0f7e9ecd01a61db239f5d7935a4ca9cc03d645
石博孚 <shibofu@asiasea.cn>
2019-02-11 18:20:42 +0800

    配置域名

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
 export default {
-    basePath: 'http://192.168.168.118:8080/',
+    basePath: 'https://www.fuloveyi.cn:8080/',
     appId:'wxe3795da8f740394d',
     appSecret:'8bea4a68389ed57102f5839147c98f53'
 }
